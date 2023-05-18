ae7f7d4f8485b71c8c3012a664a2374306dcc6c6
Julio Barreto <juliocezarjr@gmail.com>
2023-05-18 19:39:39 -0300

    backend básico para início da programação

---
+++ api/index.cjs
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 3000;
+
+
+app.get("/teste", (req, res)=> {res
+    .status(200)
+    .send({mensagem: "Deu certo"});
+})
+
+app.listen(port, () => console.log(`servidor rodando na porta ${port}`));
+
+module.exports = app
